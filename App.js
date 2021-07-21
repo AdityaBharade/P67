1b8c8d42e6e92eb36b4d89764fc73e49a2c8f99c
AdityaBharade <75526421+AdityaBharade@users.noreply.github.com>
2021-07-21 18:57:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Component } from 'react';
+import { render } from 'react-dom';
+
+import {createBottomTabNavigator} from "react-navigation-tabs"
+import {createAppContainer} from "react-navigation"
+
+import TransactionScreen from './screens/transaction';
+import SearchScreen from './screens/search';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
